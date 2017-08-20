2966e9adf0aefee23853582fdd7c561a543d7ffa
James Chai <cth451@gmail.com>
2017-08-20 13:19:51 +0800

    server side stub

--- cgi-bin/keepalive-server.sh
+++ cgi-bin/keepalive-server.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+RET=""
+
+echo "Content-type: text/html"
+echo ""
+
+echo "<html><head>"
+echo "<title>Test</title>"
+echo "<link rel=\"stylesheet\" href=\"static/tui.css\">"
+echo "</head><body>"
+echo "Welcome to your Bash CGI!"
+echo "</body></html>"
