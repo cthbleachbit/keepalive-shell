219aade8aa7c337a40f87dfaaad6544ffb56c412
James Chai <cth451@gmail.com>
2017-08-20 13:50:16 +0800

    use server root reference

--- cgi-bin/keepalive-server.sh
+++ cgi-bin/keepalive-server.sh
@@ -7,7 +7,7 @@ echo ""
 
 echo "<html><head>"
 echo "<title>Test</title>"
-echo "<link rel=\"stylesheet\" href=\"static/tui.css\">"
+echo "<link rel=\"stylesheet\" href=\"/static/tui.css\">"
 echo "</head><body>"
 echo "Welcome to your Bash CGI!"
 echo "</body></html>"
